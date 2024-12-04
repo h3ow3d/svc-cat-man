5d8b671cddb73fa327da98c94fb910821da8f0cf
h3ow3d <holdens.uk@proton.me>
2024-12-04 22:51:00 +0000

    feat: checkov ignores

--- main.tf
+++ main.tf
@@ -26,6 +26,13 @@ resource "random_string" "suffix" {
 }
 
 resource "aws_s3_bucket" "product_template_storage" {
+  # checkov:skip=CKV2_AWS_62:Ensure S3 buckets should have event notifications enable
+  # checkov:skip=CKV2_AWS_6:Ensure that S3 bucket has a Public Access block
+  # checkov:skip=CKV2_AWS_61:Ensure that an S3 bucket has a lifecycle configuration
+  # checkov:skip=CKV_AWS_18:Ensure the S3 bucket has access logging enabled
+  # checkov:skip=CKV_AWS_145:Ensure that S3 buckets are encrypted with KMS by default
+  # checkov:skip=CKV_AWS_144:Ensure that S3 bucket has cross-region replication enabled
+  # checkov:skip=CKV_AWS_21:Ensure all data stored in the S3 bucket have versioning enabled
   bucket        = lower("${var.environment}-product-template-storage-${random_string.suffix.result}")
   force_destroy = true
 
