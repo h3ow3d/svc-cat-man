f8e31242c9d4489636b3ba2f075c04eba2a68b31
h3ow3d <holdens.uk@proton.me>
2024-12-18 19:26:01 +0000

    fix(product): pipeline service role permissions

--- terraform/modules/product_pipeline/codepipeline.tf
+++ terraform/modules/product_pipeline/codepipeline.tf
@@ -110,7 +110,7 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "codepipeline_role" {
-  name               = "test-role"
+  name               = "${var.name}-codepipeline-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
