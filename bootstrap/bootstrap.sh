3bb11eeab1fc8f5cd72f2392dcc326569a37fdb7
h3ow3d <holdens.uk@proton.me>
2024-12-22 20:48:02 +0000

    fix(devcontainer): support codeconnection link open on alpine, xdg-utils

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Setup phase: Assign Account ID and AWS_REGION to variables
-echo "Assigning AWS_ACCOUNT_ID and AWS_REGION to variables..."
+echo "+++ Assigning AWS_ACCOUNT_ID and AWS_REGION to variables..."
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 AWS_REGION="eu-west-2"
 STACK_NAME="SvcCatMan"
 
 # Deploy the CloudFormation stack
-echo "Deploying the CloudFormation stack for pre-requisites..."
+echo "+++ Deploying the CloudFormation stack for pre-requisites..."
 aws cloudformation deploy \
   --template-file bootstrap/001-pre-requisites.yaml \
   --stack-name "${STACK_NAME}-pre-requisites" \
@@ -15,21 +15,23 @@ aws cloudformation deploy \
   --region "$AWS_REGION"
 
 # Get the output value of GitHubCodeConnection from the deployed stack
-echo "Retrieving the GitHubCodeConnection from the deployed stack..."
+echo "+++ Retrieving the GitHubCodeConnection from the deployed stack..."
 GITHUB_CODE_CONNECTION=$(aws cloudformation describe-stacks \
   --stack-name "${STACK_NAME}-pre-requisites" \
   --query "Stacks[0].Outputs[?OutputKey=='GitHubCodeConnection'].OutputValue" \
   --output text)
 
 # Extract the connection ID from the ARN
-echo "Extracting the connection ID from the GitHubCodeConnection ARN..."
+echo "+++ Extracting the connection ID from the GitHubCodeConnection ARN..."
 CODE_CONNECTION_ID=$(echo "$GITHUB_CODE_CONNECTION" | awk -F "/" '{print $NF}')
 
-# Open the CodeSuite connection URL in the default browser
-echo "Opening the CodeSuite connection URL in the default browser..."
-ConnectionURL="https://$AWS_REGION.console.aws.amazon.com/codesuite/settings/$AWS_ACCOUNT_ID/$AWS_REGION/codeconnections/connections/$CODE_CONNECTION_ID"
+# Open the Code Connection URL in the default browser
+echo "+++ Opening the Code Connection URL in the default browser..."
+CODE_CONNECTION_URL="https://$AWS_REGION.console.aws.amazon.com/codesuite/settings/$AWS_ACCOUNT_ID/$AWS_REGION/codeconnections/connections/$CODE_CONNECTION_ID"
 
-if command -v open &> /dev/null; then
+if command -v xdg-open &> /dev/null; then
+  xdg-open "$ConnectionURL"
+elif command -v open &> /dev/null; then
   open "$ConnectionURL"
 else
   echo "Please manually open the following URL to approve the connection:"
@@ -84,7 +86,7 @@ aws codeconnections create-sync-configuration \
   --connection-arn "$GITHUB_CODE_CONNECTION" \
   --repository-link-id "$REPOSITORY_LINK_ID" \
   --branch "main" \
-  --config-file "config.yaml" \
+  --config-file "gitsync-deployment-file.yaml" \
   --resource-name "${STACK_NAME}-management-pipeline" \
   --role-arn "arn:aws:iam::${AWS_ACCOUNT_ID}:role/SvcCatManSyncRole" \
   --sync-type "CFN_STACK_SYNC" \
